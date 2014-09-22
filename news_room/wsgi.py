9baa02fb5ef848c608fe175aed7b33348e37732c
chuckcwh <chuckcwh@gmail.com>
2014-09-22 11:39:31 -0700

    my django app

--- news_room/wsgi.py
+++ news_room/wsgi.py
@@ -12,8 +12,5 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "news_room.settings")
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-application = get_wsgi_application()
-
-
 
 application = Cling(get_wsgi_application())
